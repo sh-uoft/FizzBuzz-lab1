23c96d978426b69e34954e9ce0c0763dc8564c77
4sunnyh <4sunnyh@gmail.com>
2025-09-08 10:40:00 -0400

    Added default overload

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.print(multiples(1000, 3, 5));
+        System.out.print(multiples());
     }
 
     public static int multiples(int n, int a, int b) {
@@ -14,5 +14,8 @@ public class Multiples {
         } else {
             return n / a + n / b - n / (a * b);
         }
+    }
+    public static int multiples() {
+        return multiples(1000, 3, 5);
     }
 }
