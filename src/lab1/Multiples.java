1b08c53b8c21fe352af4b64353b151c883c8d3e8
4sunnyh <4sunnyh@gmail.com>
2025-09-08 10:27:28 -0400

    Fixed the bug

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,6 +2,17 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.print(1000 / 3 + 1000 / 5 - 1000 / (3 * 5));
+        System.out.print(multiples(1000, 3, 5));
+    }
+
+    public static int multiples(int n, int a, int b) {
+        n -= 1;
+        if (a % b == 0) {
+            return n / b;
+        } else if (b % a == 0) {
+            return n / a;
+        } else {
+            return n / a + n / b - n / (a * b);
+        }
     }
 }
